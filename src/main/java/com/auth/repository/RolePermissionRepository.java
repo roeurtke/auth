d6b0def739da4d67c4c7ec1230733bc4170c48bd
roeurtke <kesey.roeurth@gmail.com>
2025-11-14 19:12:31 +0000

    Added comment for RolePermissionRepository.

--- src/main/java/com/auth/repository/RolePermissionRepository.java
+++ src/main/java/com/auth/repository/RolePermissionRepository.java
@@ -4,6 +4,10 @@ import com.auth.model.RolePermission;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
 
+/**
+ * @author Roeurt Kesei
+ * Repository for managing RolePermission entities.
+ */
 public interface RolePermissionRepository extends ReactiveCrudRepository<RolePermission, Long> {
     
     Flux<RolePermission> findByRoleId(Long roleId);
